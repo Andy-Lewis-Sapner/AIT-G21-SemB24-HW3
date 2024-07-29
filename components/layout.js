7e1f8be47a842d3f8ac614bea43b80ef5317b6e1
Andy-Lewis-Sapner <155977415+Andy-Lewis-Sapner@users.noreply.github.com>
2024-07-29 14:28:08 +0300

    trying another change to layout

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Footer from "@/components/footer"
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-[100vh] h-fit">
       <Header />
       <main className="grow bg-gray-100 dark:bg-gray-900">{children}</main>
       <Footer />
