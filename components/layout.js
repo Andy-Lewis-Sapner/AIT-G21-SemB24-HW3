a5bbbd9cdcc1248c129660e76f03e874705debd5
Andy-Lewis-Sapner <155977415+Andy-Lewis-Sapner@users.noreply.github.com>
2024-07-29 14:24:41 +0300

    changed h-screen to min-h-screen

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Footer from "@/components/footer"
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="grow bg-gray-100 dark:bg-gray-900">{children}</main>
       <Footer />
