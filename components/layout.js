dae0c054440632de419ed41210d8d4d936b053a6
Andy-Lewis-Sapner <155977415+Andy-Lewis-Sapner@users.noreply.github.com>
2024-07-29 14:02:17 +0300

    changed layout.js main -> grow

--- components/layout.js
+++ components/layout.js
@@ -3,9 +3,9 @@ import Footer from "@/components/footer"
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-screen">
       <Header />
-      <main className="flex-grow bg-gray-100 dark:bg-gray-800">{children}</main>
+      <main className="grow bg-gray-100 dark:bg-gray-800">{children}</main>
       <Footer />
     </div>
   )
